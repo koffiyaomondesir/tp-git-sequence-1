30387263109121eafd10447b490e455e3ce811ff
koffiyaomondesir <koffiyaomondesir324@gmail.com>
2026-05-04 14:02:04 +0000

    Implémentation du message de bienvenue

--- bienvenue.cpp
+++ bienvenue.cpp
@@ -1,8 +1,9 @@
-// TODO Indiquer ce que fait le programme
+// Affiche un message de bienvenue
+#include <iostream>
 
 int main()
 {
-  // TODO Afficher un message de bienvenue
+  std::cout << "Bienvenue le monde !" << std::endl;
 
   return 0;
 }

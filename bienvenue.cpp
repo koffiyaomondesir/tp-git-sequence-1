4fd3a30e0ff406a6b3f336ceb49568bf736cf72f
koffiyaomondesir <koffiyaomondesir324@gmail.com>
2026-05-05 09:28:32 +0000

    Programme principal avec arguments de la ligne de commande

--- bienvenue.cpp
+++ bienvenue.cpp
@@ -1,9 +1,20 @@
 // Affiche un message de bienvenue
 #include "fonction-bienvenue.h"
+#include <iostream>
+#include <cstdlib>
 
-int main()
+int main(int argc, char *argv[])
 {
-  afficherBienvenue();
+  std::string message = "Bienvenue le monde !";
+  int nbAffichage = 1;
+
+  if (argc >= 2)
+    message = argv[1];
+
+  if (argc >= 3)
+    nbAffichage = atoi(argv[2]);
+
+  afficherBienvenue(message, nbAffichage);
 
   return 0;
 }
